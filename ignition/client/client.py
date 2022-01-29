bab868a3acccfa9d750b2d447b16ef39027e140c
eliaseriksson <eliaseriksson95@gmail.com>
2022-01-29 16:33:21 +0100

    commented client.py.

--- ignition/client/client.py
+++ ignition/client/client.py
@@ -15,12 +15,25 @@ logger = get_logger(__name__, logging.INFO, stdout=True)
 
 
 def setup_socket() -> socket.socket:
+    """
+    helper function for setting up the tcp socket.
+    """
     sock = socket.socket()
     sock.setblocking(False)
     return sock
 
 
 class Client:
+    """
+    ignition execution client.
+
+    the server will open a docker container with one of these clients.
+    this client will attempt to connect to the server and get a request.
+
+    the request is then process and a response is sent back.
+
+    after the client dies so should the process and docker container.
+    """
     loop: asyncio.AbstractEventLoop
     communicator: Communicator
     sock: socket.socket
@@ -34,6 +47,16 @@ class Client:
         self.connected = False
 
     async def handle_connection(self, connection: socket.socket) -> None:
+        """
+        processing of requests.
+
+        receives a reqeust from the server to process.
+        the requests' language is checked against the supported languages.
+        if its supported the source is written to a file and passed down to the Language process method.
+
+        a failed request will only send a status back to the server.
+        a successful request will send a status followed by a response.
+        """
         request = await self.communicator.recv_request(connection)
         if (language := request["language"]) in Languages:
             with tempfile.TemporaryDirectory() as tempdir:
@@ -54,9 +77,18 @@ class Client:
             await self.communicator.send_status(connection, protocol.Status.not_implemented)
 
     async def run(self) -> None:
+        """
+        connects to the server and starts the handling.
+
+        attempts to connect to the server. if any error occurs the error is logged.
+        the errors if they occur should mostly be about connection errors.
+
+        passes the connection to the handle function
+        """
         logger.info("client running...")
         try:
             connection = setup_socket()
+            # domain name set by docker. will error if run outside of docker.
             await self.loop.sock_connect(connection, ("host.docker.internal", 6090))
             await self.handle_connection(connection)
         except Exception as e:
