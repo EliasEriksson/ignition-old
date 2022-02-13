65455a932beb7f4a331119d040e677f49cf3b725
eliaseriksson <eliaseriksson95@gmail.com>
2022-02-13 23:09:48 +0100

    urlencodes username, password and db name so special characters can be used as username, pw and db name

--- sql/database.py
+++ sql/database.py
@@ -1,6 +1,7 @@
 import json
+from urllib.parse import quote_plus
 from pathlib import Path
-from sqlalchemy import create_engine, DDL
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,8 +10,8 @@ with Path(__file__).parent.joinpath(".credentials.json").open("r") as file:
     cred = json.load(file)
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{cred['role']}:{cred['password']}@"
-    f"{cred['host']}:{cred['port']}/{cred['db']}"
+    f"postgresql://{quote_plus(cred['role'])}:{quote_plus(cred['password'])}@"
+    f"{cred['host']}:{cred['port']}/{quote_plus(cred['db'])}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
